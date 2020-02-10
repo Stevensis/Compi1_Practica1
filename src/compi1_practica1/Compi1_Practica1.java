b3842fa1816964d7197359a0fbe7f8b64d280d7a
Steven_sis <aaronsishernandez@gmail.com>
2020-02-09 20:43:36 -0600

    Menu archivo finish

--- src/compi1_practica1/Compi1_Practica1.java
+++ src/compi1_practica1/Compi1_Practica1.java
@@ -16,18 +16,17 @@ public class Compi1_Practica1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       CargaA m = new CargaA();
-       LeeFichero l = new LeeFichero();
-      // l.lee("Er");
+      /* CargaA m = new CargaA();
        String obtener = m.abrirArchivo("er");
-       
         System.out.println(obtener);
         System.out.println(m.getNombreR());
         obtener += "asd Provemos sa mierda \n Ya no se que mas poner \n alv";
         System.out.println(m.getNombreA());
         m.guardarComoArchivo(obtener, ".er");
         m.guardarArchivo(obtener, m.getNombreR());
-        System.out.println("\n se guardo archivo");
+        System.out.println("\n se guardo archivo"); */
+        Ventana ventana = new Ventana();
+        ventana.show();
     }
 
     
